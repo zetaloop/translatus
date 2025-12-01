dabb95f11bbac86a3abc1fb1dc38d91a4bcb1fbf
zetaloop <zetaloop@outlook.com>
2025-12-01 20:44:26 +0800

    fix: set thread pool for Windows in vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,16 @@
+import process from 'node:process'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vitest/config'
 
 import { WxtVitest } from 'wxt/testing'
 
+const isWindows = process.platform === 'win32'
+
 export default defineConfig({
   // TODO: remove any
   plugins: [WxtVitest() as any, react()],
   test: {
+    ...(isWindows ? { pool: 'threads' } : {}),
     environment: 'node',
     globals: true,
     setupFiles: 'vitest.setup.ts',
